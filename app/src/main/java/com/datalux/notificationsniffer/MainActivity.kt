b26b467d40bf2d5c0c2172c33f21e9e64d12a1b7
Datalux <giuseppe.criscione@gmail.com>
2021-05-21 00:01:56 +0200

    feat: added example app

--- app/src/main/java/com/datalux/notificationsniffer/MainActivity.kt
+++ app/src/main/java/com/datalux/notificationsniffer/MainActivity.kt
@@ -1,58 +1,48 @@
 package com.datalux.notificationsniffer
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.provider.Settings
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.datalux.notificationsniffer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val intent = Intent().apply {
+            action = Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
 
-        setSupportActionBar(binding.toolbar)
+        if(!hasPermission(this))
+            startActivity(intent)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        NotificationSniffer.startSniffing()
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        NotificationSniffer.callback = object : SniffCallback {
+            override fun onResult(extractedNotification: ExtractedNotification) {
+                Log.d("onMain", extractedNotification.toString())
+
+            }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    private fun hasPermission(ctx: Context): Boolean {
+        val cn = ComponentName(ctx, Sniffer::class.java)
+        val flat = Settings.Secure.getString(ctx.contentResolver, "enabled_notification_listeners")
+        return flat != null && flat.contains(cn.flattenToString())
     }
 }
+
+
